30bb55df1916d8a1594b0ce42ebfd783c04d65e9
Daniel Pressner <daniel.pressner@gmail.com>
2026-03-13 10:11:00 +0100

    Initial Commit

---
+++ FocusFuel/FocusFuelApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFuelApp.swift
+//  FocusFuel
+//
+//  Created by Daniel Pressner on 13.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct FocusFuelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
